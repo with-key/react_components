560daeabfa422e71aa8f80c3fbc4f8c3e1e44f21
with-key <rem9238@kakao.com>
2023-01-11 00:57:11 +0900

    buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import styled from "styled-components";
+import Button from "./components/button/Button";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Flex row={false} gap={10}>
+      <h1>Button</h1>
+      <Flex gap={10}>
+        <Button.Primary
+          size="large"
+          outlined
+          onClick={() => window.alert("버튼을 만들어보세요")}
+        >
+          Large Primary Button
+        </Button.Primary>
+        <Button.Primary size="medium">Medium</Button.Primary>
+        <Button.Primary>Small</Button.Primary>
+      </Flex>
+
+      <Flex gap={10}>
+        <Button.Negative
+          size="large"
+          outlined
+          onClick={() => console.log(window.prompt("어렵나요?"))}
+        >
+          Large Negative Button
+        </Button.Negative>
+        <Button.Negative size="medium">Medium</Button.Negative>
+        <Button.Negative>Small</Button.Negative>
+      </Flex>
+    </Flex>
+  );
 };
 
 export default App;
+
+const Flex = styled.div`
+  display: flex;
+  flex-direction: ${({ row = "row" }) => (row ? "row" : "column")};
+  gap: ${({ gap }) => `${gap}px`};
+`;
--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,7 +1,94 @@
 import React from "react";
+import styled, { css } from "styled-components";
 
-const Button = () => {
-  return <div>Button</div>;
+const PrimitiveButton = ({ children, ...rest }) => {
+  return <StyledButton {...rest}>{children}</StyledButton>;
 };
 
+const PrimaryButton = (props) => {
+  return (
+    <PrimitiveButton
+      {...props}
+      bc="#55efc4"
+      color="#000000"
+      activeBc="#00b894"
+    />
+  );
+};
+
+const NegativeButton = (props) => {
+  return (
+    <PrimitiveButton
+      {...props}
+      bc="#fab1a0"
+      color="#d63031"
+      activeBc="#e17055"
+    />
+  );
+};
+
+const Primary = PrimaryButton;
+const Negative = NegativeButton;
+
+const Button = Object.assign(
+  {},
+  {
+    Primary,
+    Negative,
+  }
+);
+
 export default Button;
+
+const StyledButton = styled.button`
+  border: none;
+  cursor: pointer;
+
+  border-radius: 8px;
+  background-color: ${({ bc }) => bc};
+  color: ${({ color }) => color};
+  font-weight: ${({ fw }) => fw};
+
+  &:active {
+    background-color: ${({ activeBc }) => activeBc};
+  }
+
+  ${({ size }) => {
+    switch (size) {
+      case "large":
+        return css`
+          height: 50px;
+          width: 200px;
+        `;
+      case "medium":
+        return css`
+          height: 45px;
+          width: 130px;
+        `;
+      case "small":
+        return css`
+          height: 40px;
+          width: 100px;
+        `;
+      default:
+        return css`
+          height: 40px;
+          width: 100px;
+        `;
+    }
+  }}
+
+  ${({ outlined, bc }) => {
+    if (outlined) {
+      return css`
+        border: 3px solid ${bc};
+        background-color: #fff;
+        font-weight: 600;
+
+        &:active {
+          background-color: #eeeeee;
+        }
+      `;
+    }
+  }}
+`;
