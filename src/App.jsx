904816d1dcebdb9743aa07d874cb20ffcaca4c42
with-key <rem9238@kakao.com>
2023-01-11 01:49:47 +0900

    add: format input

--- src/App.jsx
+++ src/App.jsx
@@ -2,37 +2,44 @@ import React from "react";
 import styled from "styled-components";
 import Button from "./components/button/Button";
 import { IconArrow, IconBell } from "./components/button/Icon";
+import Input from "./components/input/Input";
 
 const App = () => {
   return (
-    <Flex row={false} gap={10}>
-      <h1>Button</h1>
-      <Flex gap={10}>
-        <Button.Primary
-          rightSlot={<IconArrow color={"#000000"} />}
-          size="large"
-          outlined
-          onClick={() => window.alert("버튼을 만들어보세요")}
-        >
-          Large Primary Button
-        </Button.Primary>
-        <Button.Primary size="medium">Medium</Button.Primary>
-        <Button.Primary>Small</Button.Primary>
-      </Flex>
+    <>
+      <Flex row={false} gap={10}>
+        <h1>Button</h1>
+        <Flex gap={10}>
+          <Button.Primary
+            rightSlot={<IconArrow color={"#000000"} />}
+            size="large"
+            outlined
+            onClick={() => window.alert("버튼을 만들어보세요")}
+          >
+            Large Primary Button
+          </Button.Primary>
+          <Button.Primary size="medium">Medium</Button.Primary>
+          <Button.Primary>Small</Button.Primary>
+        </Flex>
 
-      <Flex gap={10}>
-        <Button.Negative
-          size="large"
-          outlined
-          rightSlot={<IconBell />}
-          onClick={() => console.log(window.prompt("어렵나요?"))}
-        >
-          Large Negative Button
-        </Button.Negative>
-        <Button.Negative size="medium">Medium</Button.Negative>
-        <Button.Negative>Small</Button.Negative>
+        <Flex gap={10}>
+          <Button.Negative
+            size="large"
+            outlined
+            rightSlot={<IconBell />}
+            onClick={() => console.log(window.prompt("어렵나요?"))}
+          >
+            Large Negative Button
+          </Button.Negative>
+          <Button.Negative size="medium">Medium</Button.Negative>
+          <Button.Negative>Small</Button.Negative>
+        </Flex>
       </Flex>
-    </Flex>
+      <div>
+        <h1>Price Format Input</h1>
+        <Input></Input>
+      </div>
+    </>
   );
 };
 
--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
 
 const Input = () => {
-  return <div>Input</div>;
+  const [value, setValue] = useState({
+    raw: "0",
+    format: "0",
+  });
+
+  return (
+    <>
+      <StyledInput
+        type="text"
+        value={value.format}
+        onChange={({ target }) => {
+          const rex = /\D/g;
+          if (!rex.test(target.value.replaceAll(",", ""))) {
+            setValue((old) => ({
+              ...old,
+              raw: target.value.replaceAll(",", ""),
+              format: new Intl.NumberFormat().format(
+                target.value.replaceAll(",", "")
+              ),
+            }));
+          }
+        }}
+      />
+      <br />
+      <br />
+      <div>
+        <div>
+          <code>format: {value.format}</code>
+        </div>
+        <div>
+          <code>raw : {value.raw}</code>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Input;
+const StyledInput = styled.input`
+  border: 1px solid #333333;
+  height: 40px;
+  width: 200px;
+  outline: none;
+  border-radius: 8px;
+  padding-left: 12px;
+  padding-right: 12px;
+
+  &:focus-within {
+    box-shadow: 0 0 0 1px #000;
+  }
+`;
