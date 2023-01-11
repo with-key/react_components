0ee31a900184d7b914e73becf11ca03937cab3e4
with-key <rem9238@kakao.com>
2023-01-11 22:19:57 +0900

    modal portal 적용

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import Button from "./components/button/Button";
 import { IconArrow, IconBell } from "./components/button/Icon";
 import Input from "./components/input/Input";
+import * as Modal from "./components/modal/Modal";
 
 const App = () => {
   return (
@@ -39,6 +40,52 @@ const App = () => {
         <h1>Price Format Input</h1>
         <Input></Input>
       </div>
+      <div>
+        <h1>Modal</h1>
+        <Flex gap={10}>
+          <Modal.Root>
+            <Modal.Trigger asChild>
+              <Button.Primary>open modal</Button.Primary>
+            </Modal.Trigger>
+            <Modal.Portal>
+              <Modal.Overlay onClose />
+              <ModalContent>
+                <div>닫기와 확인 버튼이 모두 있는 모달이네요.</div>
+                <ModalButtonSetter>
+                  <Modal.Close asChild>
+                    <Button.Negative>닫기</Button.Negative>
+                  </Modal.Close>
+                  <Button.Primary
+                    onClick={() => {
+                      console.log("on!");
+                    }}
+                  >
+                    확인
+                  </Button.Primary>
+                </ModalButtonSetter>
+              </ModalContent>
+            </Modal.Portal>
+          </Modal.Root>
+          <Modal.Root>
+            <Modal.Trigger asChild>
+              <Button.Negative size="large" outlined>
+                open modal
+              </Button.Negative>
+            </Modal.Trigger>
+            <Modal.Portal>
+              <Modal.Overlay onClose />
+              <ModalContent>
+                <div>닫기만 있는 모달이네요.</div>
+                <ModalButtonSetterSecond>
+                  <Modal.Close asChild>
+                    <Button.Negative>X</Button.Negative>
+                  </Modal.Close>
+                </ModalButtonSetterSecond>
+              </ModalContent>
+            </Modal.Portal>
+          </Modal.Root>
+        </Flex>
+      </div>
     </>
   );
 };
@@ -49,4 +96,30 @@ const Flex = styled.div`
   display: inline-flex;
   flex-direction: ${({ row = "row" }) => (row ? "row" : "column")};
   gap: ${({ gap }) => `${gap}px`};
+`;
+
+const ModalContent = styled(Modal.Content)`
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  border-radius: 12px;
+  box-sizing: border-box;
+  padding: 24px;
+  background-color: #fff;
+  width: 500px;
+  height: 300px;
+`;
+
+const ModalButtonSetter = styled.div`
+  position: absolute;
+  bottom: 12px;
+  right: 12px;
+  display: flex;
+  gap: 5px;
+`;
+
+const ModalButtonSetterSecond = styled.div`
+  position: absolute;
+  top: 12px;
+  right: 12px;
 `;
--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,7 +1,115 @@
-import React from "react";
+import React, { createContext, useContext, useState } from "react";
+import ReactDOM from "react-dom";
+import styled from "styled-components";
 
-const Modal = () => {
-  return <div>Modal</div>;
+const ModalContext = createContext({});
+
+/*--------------------------------------------------------*
+ * Modal Root
+ *--------------------------------------------------------*/
+
+const ModalRoot = ({ children }) => {
+  const [open, setOpen] = useState(false);
+  return (
+    <ModalContext.Provider
+      value={{
+        open,
+        setOpen,
+      }}
+    >
+      {children}
+    </ModalContext.Provider>
+  );
 };
 
-export default Modal;
+/*--------------------------------------------------------*
+ * Modal Trigger
+ *--------------------------------------------------------*/
+
+const ModalTrigger = ({ children, asChild, ...rest }) => {
+  const { setOpen } = useContext(ModalContext);
+  return asChild ? (
+    <div onClick={() => setOpen((pre) => !pre)} {...rest}>
+      {children}
+    </div>
+  ) : (
+    <button onClick={() => setOpen((pre) => !pre)} {...rest}>
+      {children}
+    </button>
+  );
+};
+
+/*--------------------------------------------------------*
+ * Modal Portal
+ *--------------------------------------------------------*/
+
+const ModalPortal = ({ children }) => {
+  const portalTarget = document.getElementById("portal-target");
+  console.log(portalTarget);
+
+  if (!portalTarget) {
+    return null;
+  }
+
+  return ReactDOM.createPortal(children, portalTarget);
+};
+
+/*--------------------------------------------------------*
+ * Modal Overlay
+ *--------------------------------------------------------*/
+
+const ModalOverlay = ({ onClose }) => {
+  const { open, setOpen } = useContext(ModalContext);
+  return open ? (
+    <StyledOverlay onClick={() => (onClose ? setOpen(false) : null)} />
+  ) : (
+    <></>
+  );
+};
+
+const StyledOverlay = styled.div`
+  width: 100%;
+  height: 100vh;
+  inset: 0;
+  position: fixed;
+  background-color: #ddd;
+`;
+
+/*--------------------------------------------------------*
+ * Modal Content
+ *--------------------------------------------------------*/
+
+const ModalContent = ({ children, ...rest }) => {
+  const { open } = useContext(ModalContext);
+  return open ? <StyledContent {...rest}>{children}</StyledContent> : <></>;
+};
+
+const StyledContent = styled.div`
+  position: absolute;
+`;
+
+/*--------------------------------------------------------*
+ * Modal Close
+ *--------------------------------------------------------*/
+
+const ModalClose = ({ children, asChild, ...rest }) => {
+  const { setOpen } = useContext(ModalContext);
+  return asChild ? (
+    <div onClick={() => setOpen(false)} {...rest}>
+      {children}
+    </div>
+  ) : (
+    <button onClick={() => setOpen(false)} {...rest}>
+      {children}
+    </button>
+  );
+};
+
+const Root = ModalRoot;
+const Trigger = ModalTrigger;
+const Portal = ModalPortal;
+const Overlay = ModalOverlay;
+const Close = ModalClose;
+const Content = ModalContent;
+
+export { Root, Trigger, Portal, Overlay, Close, Content };
